87400a256017743a1325ca8e618e3df645f590ab
Jorge Rubio <jor.rubioa@duocuc.cl>
2022-10-26 07:23:29 -0300

    prueba unitaria

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Presentation;
+using domains;
+using Domain;
+using Common1.Cache;
+using System.Runtime.Remoting.Messaging;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void mostrarAccident()
+        {
+            CN_Accidentabilidad objeto = new CN_Accidentabilidad();
+
+            Assert.IsNotNull(objeto);
+
+        }
+
+        
+        
+    }
+}
